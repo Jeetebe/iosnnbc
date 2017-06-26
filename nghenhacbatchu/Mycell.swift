dc8d4f368cf9309360748b4a8dceb4eeba483482
MacBook <macbook@Macbook-Pro.local>
2017-06-26 22:50:42 +0700

    first commit

---
+++ nghenhacbatchu/Mycell.swift
@@ -0,0 +1,15 @@
+//
+//  Mycell.swift
+//  nghenhacbatchu
+//
+//  Created by MacBook on 6/26/17.
+//  Copyright © 2017 MacBook. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class  Mycell: UICollectionViewCell{
+    
+    @IBOutlet weak var lb2: UILabel!
+}
--- nghenhacbatchu/ViewController.swift
+++ nghenhacbatchu/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collv: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Set the number of items in your collection view.
+        return 18
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Access
+        let cell = collv.dequeueReusableCell(withReuseIdentifier: "mycell2", for: indexPath) as! Mycell
+        // Do any custom modifications you your cell, referencing the outlets you defined in the Custom cell file.
+        cell.backgroundColor = UIColor.white
+        cell.lb2.text = "\(indexPath.item)"
+        
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        
+        return 4
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        
+        return 4
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        let screenSize: CGRect = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        let s=(screenWidth-32-4*6-50)/6
+        
+        let cellSize = CGSize(width:s , height:s)
+        return cellSize
+    }
 }
 
